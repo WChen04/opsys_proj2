733909b82089ed459c06bded29e0e2936ebd5a3b
William Chen <williamchen259@gmail.com>
2024-08-16 06:24:19 -0400

    recent changes

--- src/project.py
+++ src/project.py
@@ -89,7 +89,7 @@ def populateProcess( process_type: str, curr_process: Process ):
     curr_process.defineCpuBursts(cpu_bursts)
 
     s = 's' if cpu_bursts > 1 else ''
-    print(f"{process_type} process {curr_process.process_name}: arrival time {arrival_time}ms; {cpu_bursts} CPU burst{s}:")
+    print(f"{process_type} process {curr_process.process_name}: arrival time {arrival_time}ms; {cpu_bursts} CPU burst{s}")
     
     for i in range( cpu_bursts ):
         cpu_burst_time = math.ceil(next_exp(lambda_param, upperBound))        
@@ -104,13 +104,110 @@ def populateProcess( process_type: str, curr_process: Process ):
                 io_burst_time *= 8
             curr_process.addIOBurstTime(io_burst_time)
             TOTAL_IO_burst_time += io_burst_time
-                
-            print(f"==> CPU burst {cpu_burst_time:.0F}ms ==> I/O burst {io_burst_time}ms")
-        else:
-            print(f"==> CPU burst {cpu_burst_time:.0F}ms")
+            
+        #     print(f"==> CPU burst {cpu_burst_time:.0F}ms ==> I/O burst {io_burst_time}ms")
+        # else:
+        #     print(f"==> CPU burst {cpu_burst_time:.0F}ms")
+'''
+- First Come First Serve
+- Creates map of all processes 
+- Updates maps and objects
+'''
+def FCFS(processes: list, switchTime: int):
+    print("time 0ms: Simulator started for FCFS [Q empty]")
+    # Check if CPU is busy
+    isFullCpu = 0
 
-def FCFS( processes: list ):
-    return
+    # Keys are time, values are objects
+    processDict = {}
+
+    readyQueue = []
+
+    currentTime = 0
+
+    # Sort to find smallest process
+    processes.sort(key=lambda process: process.arrival_time)
+
+    # This for-loop will only last until all processes have arrived.
+    for process in processes:
+        if processDict:
+            print(f"{process.process_name}\n")
+            min_time = min(processDict)
+            processArrival = process.arrival_time
+            if min_time < processArrival:
+                min_process = processDict[min_time]
+                if len(min_process.cpu_burst_times) > 0:
+                    readyQueue.pop(0)
+                    if len(min_process.cpu_burst_times) > 1:
+                        print(f"time {min_time}ms: Process {min_process.process_name} completed a CPU burst; {len(min_process.cpu_burst_times)} bursts to go [Q {', '.join([q.process_name for q in readyQueue])}]")
+                    else:
+                         print(f"time {min_time}ms: Process {min_process.process_name} completed a CPU burst; {len(min_process.cpu_burst_times)} burst to go [Q {', '.join([q.process_name for q in readyQueue])}]")
+                    min_process.cpu_burst_times.pop(0)
+                    relaxTime = min_time + min_process.io_burst_times[0] + 2
+                    processDict[relaxTime] = min_process
+                    del processDict[min_time]
+                    print(f"time {min_time}ms: Process {min_process.process_name} switching out of CPU; blocking on I/O until time {relaxTime}ms [Q {', '.join([q.process_name for q in readyQueue])}]")
+                    isFullCpu = 0
+                    continue
+                else:
+                    print(f"time {min_time}ms: Process {min_process.process_name} terminated [Q empty]")
+                    del processDict[min_time]
+                    
+                isFullCpu = 1
+    
+        print(f"time {process.arrival_time}ms: Process {process.process_name} arrived; added to ready queue [Q {process.process_name}]")
+        readyQueue.append(process)
+        # Check if CPU is empty
+        if isFullCpu == 0:
+            processTime = process.arrival_time + int(switchTime/2)
+            processDict[processTime] = process
+            print(f"time {processTime}ms: Process {process.process_name} started using the CPU for {process.cpu_burst_times[0]}ms burst [Q empty]")
+            processDict[processTime + process.cpu_burst_times[0]] = process
+            del processDict[processTime]
+            process.cpu_burst_times.pop(0)
+            isFullCpu = 1
+       
+               
+            # print(f"time {process.arrival_time}ms: Process {process.process_name} arrived; added to ready queue [Q {', '.join([q.process_name for q in readyQueue])}]")
+            # readyQueue.append(process)
+            # processDict[process.arrival_time] = process
+           
+
+
+
+
+ 
+    # for process in processes:
+    #     if first_process == 1 and not waiting:
+    #         queue.append(process)
+    #         time += process.arrival_time
+    #         print(f"time {time}ms: Process {process.process_name} arrived and added to ready queue [Q {process.process_name}]")
+    #         time += int(time_switch/2)
+    #         queue.pop(0)
+    #         print(f"time {time}ms: Process {process.process_name} started using the CPU for {process.cpu_burst_times[0]}ms burst [Q empty]")
+    #         waiting.append(time + process.cpu_burst_times[0])
+    #         first_process = 0
+
+    #     elif waiting[0] > process.arrival_time:
+    #         time = process.arrival_time
+    #         queue.append(process)
+    #         print(f"time {time}ms: Process {process.process_name} arrived and added to ready queue [Q {', '.join([q.process_name for q in queue])}]")
+
+    #     elif waiting[0] < process.arrival_time:
+    #         time = waiting[0]
+    #         process.cpu_burst_times.pop(0)
+    #         print(f"time {time}ms: Process {process.process_name} completed a CPU burst; {len(process.cpu_burst_times)} to go [Q {', '.join([q.process_name for q in queue])}]")
+
+
+                # print(f"time {time}ms: Process {process.process_name} arrived and added to ready queue [Q", end="")
+                # for p in queue:
+                #     print(f" {p.process_name}", end="")
+                # print("]")
+                # if len(queue) == 1:
+
+
+
+                
 
 def SJF( processes: list ):
     return
@@ -180,7 +277,7 @@ if __name__ == "__main__":
         else:
             IO_bound_CPU_burst_time_total += sum( curr_process.cpu_burst_times )
             IO_bound_IO_burst_time_total += sum( curr_process.io_burst_times )
-            IO_bound_CPU_burst_total += curr_process.cpu_bursts
+            IO_bound_CPU_burst_total += curr_process.cpu_bursts     
 
     # write the output into the simout.txt
     f = open( "simout.txt", "w")
@@ -220,8 +317,10 @@ if __name__ == "__main__":
     alpha = float( sys.argv[7] )
     Tslice = int( sys.argv[8] )
 
-    print( "<<< PROJECT PART II\n" )
-    print( f"<<< -- t_cs={Tcs}; alpha={alpha}; t_slice={Tslice}ms\n")
+    print( "\n<<< PROJECT PART II" )
+    print( f"<<< -- t_cs={Tcs}ms; alpha={alpha}; t_slice={Tslice}ms")
+
+    FCFS(process_ids, Tcs)
 
     
 
